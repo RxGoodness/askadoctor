ed3fa5a60d8ed06d88e4e174927108e35280a555
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-24 23:00:04 +0100

    adding input validations

--- src/services/auth/schema.ts
+++ src/services/auth/schema.ts
@@ -5,7 +5,7 @@
  */
 
 import { string, object, literal } from "zod";
-import { parseUrl } from "../../commons";
+import { parseUrl, parseUserRole, parseUserStatus } from "../../commons";
 
 /**
  *
@@ -26,7 +26,7 @@ export default {
     email: string({ required_error: "email is required" }).email({
       message: "email format is not supported",
     }),
-    password: string({ required_error: "password is required" }),
+    password: string({ required_error: "password is required" }).min(8),
     phone_number: string({ required_error: "phone number is required" }),
     // username: string({ required_error: "username is required" }),
     // banner: string()
@@ -57,9 +57,72 @@ export default {
    *
    */
 
-  findUser: object({
+  login: object({
+    email: string({ required_error: "email is required" }).email({
+      message: "email format is not supported",
+    }),
+    password: string({ required_error: "password is required" }).min(8),
+  }),
+
+  sendUserverification: object({
     email: string({ required_error: "email is required" }).email({
       message: "email format is not supported",
     }),
   }),
+
+  verification: object({
+    email: string({ required_error: "email is required" }).email({
+      message: "email format is not supported",
+    }),
+    otp: string({ required_error: "otp is required" }).length(6, { message: 'Must be exactly 6 characters long'}),
+  }),
+
+  addDoctor: object({
+    email: string({ required_error: "email is required" }).email({
+      message: "email format is not supported",
+    }),
+    role: string({ required_error: "role is required" }).refine((val: string) => parseUserRole(val), {
+      message:
+      "Invalid value for user! only 'doctor' or 'admin' or 'user' is allowed.",
+      // "Invalid value for user! only 'doctor' is allowed.",
+    }),
+
+    status: string({ required_error: "status is required" }).refine((val: string) => parseUserStatus(val), {
+      message:
+      "Invalid value for user! only 'active' or 'inactive' user status is allowed.",
+    }),
+    password: string({ required_error: "password is required" }).min(8),
+  }),
+
+  changePassword: object({
+    role: string({ required_error: "role is required" }).refine((val: string) => parseUserRole(val), {
+      message:
+      "Invalid value for user! only 'doctor' or 'admin' or 'user' is allowed.",
+    }),
+
+    id: string({ required_error: "id is required" }),
+    password: string({ required_error: "password is required" }).min(8),
+    confirmPassword: string({ required_error: "password is required" }).min(8),
+
+  }).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  }),
+
+  resetPassword: object({
+    email: string({ required_error: "email is required" }).email({
+      message: "email format is not supported",
+    }),
+
+    otp: string({ required_error: "otp is required" }).length(6, { message: 'Must be exactly 6 characters long'}),
+    password: string({ required_error: "password is required" }).min(8),
+    confirmPassword: string({ required_error: "password is required" }).min(8),
+
+  }).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  }),
+
+
+
 };
