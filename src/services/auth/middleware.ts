421673a85afa8f67e135e06ead148d665917155e
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-24 23:00:50 +0100

    adding input validations

--- src/services/auth/middleware.ts
+++ src/services/auth/middleware.ts
@@ -35,13 +35,61 @@ export default {
    *
    */
   
-  async findUser(req: Request, res: Response, next: NextFunction) {
+  async login(req: Request, res: Response, next: NextFunction) {
     try {
-      await schema.findUser.parseAsync(req.body);
+      await schema.login.parseAsync(req.body);
       next();
     } catch (error) {
       next(error);
     }
   },
+
+  async sendUserVerification(req: Request, res: Response, next: NextFunction) {
+    try {
+      await schema.sendUserverification.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+
+  async verification(req: Request, res: Response, next: NextFunction) {
+    try {
+      await schema.verification.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async addDoctor(req: Request, res: Response, next: NextFunction) {
+    try {
+      await schema.addDoctor.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  
+
+  async changePassword(req: Request, res: Response, next: NextFunction) {
+    try {
+      await schema.changePassword.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async resetPassword(req: Request, res: Response, next: NextFunction) {
+    try {
+      await schema.resetPassword.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  
 
 };
