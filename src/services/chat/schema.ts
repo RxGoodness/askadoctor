bd3f097f26f1b8c218b91ced8b6840f85f76c7ad
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-25 00:05:27 +0100

    refactoring and adding inputs validations functions

---
+++ src/services/chat/schema.ts
@@ -0,0 +1,24 @@
+import { string, object, literal } from "zod";
+
+
+export default {
+    /**
+     *
+     * create user
+     *
+     */
+createChat: object({
+    senderId: string({ required_error: "senderId is required" }),
+  }).strict(),
+
+  addMessage: object({
+    senderId: string({ required_error: "senderId is required" }),
+    recieverId: string({ required_error: "recieverId is required" }),
+    chatId: string({ required_error: "chatId is required" }),
+    text: string({ required_error: "text is required" }),
+    
+
+  }).strict(),
+
+
+}
