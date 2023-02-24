6906826fec91dbe7e8f908b675fa9d2ef610e586
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-24 23:01:56 +0100

    addinng input validations

--- src/services/chat/route.ts
+++ src/services/chat/route.ts
@@ -1,31 +1,33 @@
-import express from 'express'
-import { Request, Response } from 'express'
-import controller from './controller';
-import { verifyAcessToken } from '../../commons';
+import express from "express";
+import { Request, Response } from "express";
+import controller from "./controller";
+import { authorizedUser, verifyAcessToken } from "../../commons";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/', [verifyAcessToken, controller.createChats]
-);
+router.post("/", [verifyAcessToken, controller.createChats]);
 
-router.get('/:userId', 
-  controller.userChatss);
+router.get("/:userId", controller.userChatss);
 
-router.get('/find/:firstId/:secondId', 
-  controller.findChats);
+router.get("/find/:firstId/:secondId", controller.findChats);
 
-router.post('/add-messages', [verifyAcessToken,
-    controller.addMessagess]);
+router.post("/add-messages", [verifyAcessToken, controller.addMessagess]);
 
-router.get('/:chatId', controller.getMessagess);
+router.get("/:chatId", controller.getMessagess);
 
-router.get('/:senderId',[verifyAcessToken, controller.getUserMessages]);
-    
-    router.post('/', controller.createPosts);
-    router.get('/:id', controller.getPosts);
-    router.put('/:id', controller.updatePosts);
-    router.delete('/:id', controller.deletePosts);
-    router.put('/:id/like', controller.likePosts);
-    // router.get('/:id/timeline', controller.getTimelinePosts);
+router.get("/:senderId", [verifyAcessToken,authorizedUser("admin", "doctor"), controller.getUserMessages]);
 
-export {router};
+router.get("/", [
+  verifyAcessToken,
+  authorizedUser("admin", "doctor"),
+  controller.getAllMessages,
+]);
+
+router.post("/", controller.createPosts);
+router.get("/:id", controller.getPosts);
+router.put("/:id", controller.updatePosts);
+router.delete("/:id", controller.deletePosts);
+router.put("/:id/like", controller.likePosts);
+// router.get('/:id/timeline', controller.getTimelinePosts);
+
+export { router };
