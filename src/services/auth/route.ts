749bc9f9a7d943bb6c93a92b87e39014a4d30ec7
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-24 23:01:14 +0100

    addinng input validations

--- src/services/auth/route.ts
+++ src/services/auth/route.ts
@@ -24,8 +24,8 @@ export const authRouter: Router = Router();
  */
 
 authRouter.post('/create-user', [middleware.createUser, controller.createUser]);
-authRouter.post('/send-verification', [controller.sendVerification]);
-authRouter.post('/verify-user', [controller.verifyUser]);
+authRouter.post('/send-verification', [middleware.sendUserVerification, controller.sendVerification]);
+authRouter.post('/verify-user', [middleware.verification, controller.verifyUser]);
 /**
  * @swagger
  * /api/auth/handshake:
@@ -49,19 +49,19 @@ authRouter.post('/verify-user', [controller.verifyUser]);
  *       400:
  *         description: An error occured while Login in
  */
-authRouter.post('/initiate-login', [middleware.findUser, controller.findUser]);
+authRouter.post('/initiate-login', [middleware.login, controller.findUser]);
 
-authRouter.post('/verify-login', [controller.verifyLogin]);
+authRouter.post('/verify-login', [middleware.verification, controller.verifyLogin]);
 
-authRouter.post('/add-doctor', [verifyAcessToken, controller.addDoctor])
+authRouter.post('/add-doctor', [verifyAcessToken, middleware.addDoctor,controller.addDoctor])
 
-authRouter.post('/change-password', [verifyAcessToken, controller.changePassword])
+authRouter.post('/change-password', [verifyAcessToken, middleware.changePassword, controller.changePassword])
 
-authRouter.post('/forget-password', [controller.fogetPassword])
+authRouter.post('/forget-password', [middleware.sendUserVerification, controller.fogetPassword])
 
-authRouter.post('/reset-password', [controller.resetPassword])
+authRouter.post('/reset-password', [middleware.resetPassword, controller.resetPassword])
 
-authRouter.post('/login', [controller.login])
+authRouter.post('/login', [middleware.login, controller.login])
 
 
 
